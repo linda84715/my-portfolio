bbc883a79e3527e0f8133d188413b7a49579cb98
Linda Lin <linda84715@gmail.com>
2025-02-01 03:09:18 +0800

    First depoly okay

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',  // 確保這裡是你的 GitHub Pages 網站路徑
 })
