b16d50c277d30246b23bb8649fbf0b52192785c0
Linda Lin <linda84715@gmail.com>
2025-02-03 05:18:05 +0800

    need to check the scroll

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     // 設置防抖延遲，防止滾動太快
     setTimeout(() => {
       setIsScrolling(false);
-    }, 3000); // 延遲時間 400ms
+    }, 600); // 延遲時間 400ms
 
     if (e.deltaY > 0) {
       // 滾動向下，顯示下一個區塊
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Linda Lin</h1>
+      <h2>Linda Lin</h2>
       <p className='hashtag'>Engineering & Computer Science | Sr. Project Management | Full-Stack Developer</p>
     </nav>
   );
