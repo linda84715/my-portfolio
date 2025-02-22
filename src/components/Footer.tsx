56823989c5cebf249de81b06f3fd42fd8c708ab2
Linda Lin <linda84715@gmail.com>
2025-02-22 20:50:14 +0000

    Updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="footer">
-        <div>Linda Lin © 2025. All Rights Reserved.</div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// vite.config.ts
-export default defineConfig({
-  base: '/my-portfolio/',  // 確保這裡是你的 GitHub Pages 路徑
-  plugins: [react()],
-});
